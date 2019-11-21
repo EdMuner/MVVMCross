b61067b64d5d3c3c77534d3a21b90a41fa866a85
EdisonMunera <edisonmunera72@gmail.com>
2019-11-21 16:54:02 -0500

    core finish

---
+++ TipCalculator.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,66 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalculator.Core.Services;
+
+namespace TipCalculator.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculationService _calculationService;
+        private decimal _subTotal;
+        private int _generosity;
+        private decimal _tip;
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public decimal Subtotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => Subtotal);
+                Recalculate();
+            }
+        }
+
+        public decimal Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);    
+            }
+        }
+
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+            }
+        }
+
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+            Subtotal = 100;
+            Generosity = 10;
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(Subtotal, Generosity);
+        }
+    }
+}
